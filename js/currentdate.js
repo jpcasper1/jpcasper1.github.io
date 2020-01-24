626306ba2b0db83335b4878aa19d4f72c826f3d0
jpcasper1 <pal18029@byui.edu>
2020-01-23 22:37:28 -0500

    commitme

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,3 +1,3 @@
 const options = {weekday: "long",day: "numeric", month: "long", year: "numeric"};
-document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
 document.getElementById("year").innerHTML = new Date().getFullYear();
+document.getElementById("currentdate").textContent = new Date(document.lastModified).toLocaleDateString("en-US",options)
