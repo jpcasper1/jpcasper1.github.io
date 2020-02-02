eaa567035811c308241526cd1db4faf2adb663ad
jpcasper1 <pal18029@byui.edu>
2020-02-01 22:23:36 -0500

    checkingoutoldcode

--- lesson4/js/todaysDate.js
+++
@@ -1,3 +0,0 @@
-const options = {day:"numeric", weekday:"long", month:"long", year:"numeric"};
-document.getElementById("year").innerHTML = new Date().getFullYear();
-document.getElementById("currentdate").textContent = new Date(document.lastModified).toLocaleDateString("en-US",options);
