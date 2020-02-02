12798da42d3b2ca2a0d49641d3dc9ecc4bed5f8a
jpcasper1 <pal18029@byui.edu>
2020-02-01 22:26:55 -0500

    lastlastfixes

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -1,12 +1,10 @@
 const hambutton = document.getElementsByClassName('ham')[0];
 
-
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 };
-
 
 const options = {weekday:"long", day:"numeric", month:"long", year:"numeric"};
 document.getElementById("currentdate").textContent = new Date().toLocaleString("en-DE",options);
