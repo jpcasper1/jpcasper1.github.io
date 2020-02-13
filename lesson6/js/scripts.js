9d5dae1d8191f49eee9819d0bd7d8d98b4349e3b
jpcasper1 <pal18029@byui.edu>
2020-02-13 11:56:22 -0500

    week6andwebfont

---
+++ lesson6/js/scripts.js
@@ -0,0 +1,15 @@
+const hambutton = document.getElementsByClassName('hammenu')[0];
+
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+};
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+const options = {weekday:"long", day:"numeric", month:"long", year:"numeric"};
+document.getElementById("currentdate").textContent = new Date().toLocaleString("en-DE",options);
+
+    var d = new Date();
+	if(d.getDay() == 5) {document.getElementById("announce").style.display="block";}
+	else{document.getElementById("announce").style.display="none";}
