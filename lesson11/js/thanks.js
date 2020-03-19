aadef960124fc531daae0dd54983a889bebb217c
jpcasper1 <pal18029@byui.edu>
2020-03-19 18:59:15 -0400

    thursdayupdate

---
+++ lesson11/js/thanks.js
@@ -0,0 +1,10 @@
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+	});
+    return vars;
+}
+
+document.getElementById('thanks').innerHTML = '<h1> Thank you, '+getUrlVars()['fname'].replace('+',' ') + ', for submitting a report.</h1>';
