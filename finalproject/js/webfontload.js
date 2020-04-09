412f21680a263a48d31e6dec6088a50d5d73b182
jpcasper1 <pal18029@byui.edu>
2020-04-09 00:04:50 -0400

    fonttiming

--- finalproject/js/webfontload.js
+++ finalproject/js/webfontload.js
@@ -1,4 +1,7 @@
 WebFont.load({
     google: {
         families: [
-            'Big Shoulders Display', 'Luckiest Guy', 'Rokkitt', 'Lilita One', 'Montserrat'        ]}});       
+            'Big Shoulders Display', 'Luckiest Guy', 'Rokkitt', 'Lilita One', 'Montserrat'        
+        ]},
+    timeout: 2000 //Set the timeout for two seconds
+});       
